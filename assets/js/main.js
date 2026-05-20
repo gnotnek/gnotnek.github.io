48bd3a4200802f54881b26101de6b2a542416c8d
Dhista Ardiansyah <dhistadna@gmail.com>
2026-05-20 16:06:31 +0700

    feat: update my protofolio website to make it looks cool

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,12 @@ function toggleVisibility(id) {
         element.style.display = 'none';
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    var yearElements = document.querySelectorAll('[data-current-year]');
+    var currentYear = new Date().getFullYear();
+
+    yearElements.forEach(function (element) {
+        element.textContent = currentYear;
+    });
+});
